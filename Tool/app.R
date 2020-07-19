6e3872b6916a8b87b77cc5d845a3a3c47d50d309
carinapeng <fpeng@college.harvard.edu>
2020-07-19 18:23:24 -0500

    Add selector for input data

--- Tool/app.R
+++ Tool/app.R
@@ -23,6 +23,8 @@ library(markdown)
 library(shiny)
 library(shinyjs)
 library(knitr)
+library(ggplot2)
+library(incidence)
 #options(shiny.maxRequestSize=2650*1024^2)
 
 
@@ -94,12 +96,17 @@ ui <- fluidPage(
             ")),
                          withMathJax(includeMarkdown("/Users/carinapeng/PAHO-LAC/welcome.md")),
                          h3(textOutput("upload")),
-                         selectInput('mydropdown', label = 'Select', choices = 'No choices here yet'),
                          hr(),
                          fluidRow(column(3, verbatimTextOutput("value"))),
                          # Display uploaded csv file
                          tableOutput("contents")
                 ),
+                tabPanel("Data",
+                         uiOutput("selectfile"),
+                         selectInput('mydropdown', label = 'Select_municipal', choices = 'No choices here yet'),
+                         uiOutput("selectfile2")
+                         
+                         ),
                 tabPanel("Individual Vunerability Index",
                          h3("Context"),
                          h4("Demographic and Socio-economic Information about the Neighborhood"),
@@ -238,9 +245,12 @@ ui <- fluidPage(
                                  }"
                          )),
                          withMathJax(includeMarkdown("/Users/carinapeng/PAHO-LAC/context.md"))
-                )
-            )
-        )))
+                ),
+                tabPanel("Statistics", 
+                         h3("Summary Statistics"),
+                         verbatimTextOutput("contents5"),
+                         tableOutput("contents2"))
+        ))))
         
     
 server <- function(input, output, session) { 
@@ -262,17 +272,35 @@ server <- function(input, output, session) {
         #return(writeLines("Uploaded File"))
     #})
     
+    output$selectfile <- renderUI({
+        if(is.null(input$file1)) {return()}
+        list(hr(), 
+             helpText("Select the files for which you need to see data and summary stats"),
+             selectInput("Select_test", "Select_test", choices=input$file1$name)
+        )
+        
+        
+    })
     
-    #output$selectfile_context <- renderUI({
-        #if(is.null(input$file1)) {return()}
-        #list(hr(),
-             #helpText("Select the coded census file"),
-             #selectInput("Select", "Select", choices=input$file1$name))
-    #})
+    output$selectfile2 <- renderUI({
+        if(is.null(input$file1)) {return()}
+        list(hr(), 
+             helpText("Select the files for which you need to see data and summary stats"),
+             selectInput("Select_test2", "Select_test2", choices=input$file1$name)
+        )
+        
+        
+    })
+    
+    output$table <- renderTable({ 
+        if(is.null(input$file1)){return()}
+        read.table(file=input$file1$datapath[input$file1$name==input$Select])
+        
+    })
     
     file01 <- reactive({
         req(input$file1)
-        read.csv(input$file1[[1, 'datapath']],
+        read.csv(file=input$file1$datapath[input$file1$name==input$Select_test],
                  header = input$header,
                  sep = input$sep,
                  quote = input$quote)
@@ -280,16 +308,15 @@ server <- function(input, output, session) {
     
     file02 <- reactive({
         req(input$file1)
-        read.csv(input$file1[[2, 'datapath']],
-        #read.csv(file=input$file1$datapath[input$file1$name==input$Select],
+        #read.csv(input$file1[[2, 'datapath']],
+        read.csv(file=input$file1$datapath[input$file1$name==input$Select_test2],
                  header = input$header,
                  sep = input$sep,
                  quote = input$quote)
     })
     
-    observeEvent(input$file1, {
-        #updateSelectInput(session, "mydropdown", label = "Select", choices = file01()$comuna)
-        updateSelectInput(session, "mydropdown", label = "Select", choices = file01()[1])
+    observeEvent(input$Select_test, {
+        updateSelectInput(session, "mydropdown", label = "Select_municipal", choices = file01()[1])
     })
     
     municipal <- reactive({
@@ -297,6 +324,14 @@ server <- function(input, output, session) {
         file01_municipal <- file01() %>%
             filter(comuna == input$mydropdown)
         return(file01_municipal)
+    })
+    
+    df <- reactive({
+        req(input$file1)
+        x = file02()
+        x[,1]<-as.Date(x[,1], "%d/%m/%Y")
+        dfR <- estimate_R(x, method = "parametric_si", config = make_config(list(mean_si = 4.8, std_si = 2.3)))
+        return(dfR)
     })
     
     contexto01 <- reactive({
@@ -771,6 +806,12 @@ server <- function(input, output, session) {
     
     output$context2 <- renderPrint({
         return(writeLines(as.character(social())))
+    })
+    
+    
+    output$contents5 <- renderPrint({
+        x <- df()$R$Mean
+        return(writeLines(c("The current reproductive number (R) is estimated to be", round(x[length(x)],digits=2))))
     })
     
     
