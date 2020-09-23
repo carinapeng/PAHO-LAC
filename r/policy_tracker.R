56ff5a6fdbe7cdaacded93b981c3946eb12f5ba1
carinapeng <fpeng@college.harvard.edu>
2020-09-23 21:12:52 +0300

    Read phsm data from google sheets

--- r/policy_tracker.R
+++ r/policy_tracker.R
@@ -1,27 +1,14 @@
 
+library(googlesheets4)
+library(magrittr)
+library(dplyr)
+
+phsm_dribble <- googledrive::drive_get("PHSM_WHO")
+
+data <- phsm_dribble %>%
+  read_sheet()
+
+saveRDS(data, "data/who_phsm.rds")
 
 
-library(readxl)
 
-setwd("Users/carinapeng/PAHO-LAC")
-
-url <- "https://www.who.int/docs/default-source/documents/phsm/20200826-phms-who-int.zip?sfvrsn=caa32ba8_2"
-# destfile <- "data/raw/policy_tracker.xlsx"
-# download.file(url, destfile)
-
-
-# /Users/carinapeng/Downloads/20200826-phms-who-int/WHO_PHSM_Cleaned_V1_20_08_26.xlsx
-
-temp <- tempfile()
-download.file(url,temp)
-data <- read.table(unz(temp, ".xlsx"))
-unlink(temp)
-
-temp <- tempfile()
-temp2 <- tempfile()
-
-download.file(url, temp)
-unzip(zipfile = temp, exdir = temp2)
-data <- read_xls(file.path(temp2))
-
-unlink(c(temp, temp2))
