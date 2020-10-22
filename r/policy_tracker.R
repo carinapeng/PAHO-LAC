38be0844c5073661d740a93c0e9dce19c2fafcc8
carinapeng <fpeng@college.harvard.edu>
2020-10-21 19:28:57 -0500

    Add policy tracker code

--- r/policy_tracker.R
+++ r/policy_tracker.R
@@ -10,5 +10,49 @@ data <- phsm_dribble %>%
 
 saveRDS(data, "data/who_phsm.rds")
 
+# policy_national <- data %>%
+#   filter(country_territory_area == "Chile") %>%
+#   filter(admin_level == "national")
+# 
+# policy_area <- data %>%
+#   filter(area_covered == "Santiago")
+# 
+# policy_join <-
+#   rbind(policy_national, policy_area)
+# 
+# social = ifelse("Social and physical distancing measures" %in% data$who_category,
+#                 0,
+#                 1)
+# isolation = ifelse("Surveillance and response measures" %in% data$who_category,
+#                    0,
+#                    1)
+# support = ifelse("Social and physical distancing measures" %in% data$who_category,
+#                  0,
+#                  1)
+# contact = ifelse("Tracing and quarantining contacts" %in% data$who_category,
+#                  0,
+#                  1)
+# gathering = ifelse("Gatherings, businesses and services" %in% data$who_category,
+#                    0,
+#                    1)
+# disturbance = ifelse("Gatherings, businesses and services" %in% data$who_category,
+#                      0,
+#                      1)
+# 
+# combined <-
+#   data.frame(value = rbind(
+#     social,
+#     isolation,
+#     support,
+#     contact,
+#     gathering,
+#     disturbance
+#   ))
+# combined_transpose <- data.frame(transpose(combined))
+# rownames(combined_transpose) <- colnames(combined)
+# colnames(combined_transpose) <- rownames(combined)
+# 
+# combined_transpose
+
 
 
